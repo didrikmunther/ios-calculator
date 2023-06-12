196266ca5feb2bcf2a6cc654371198caf9e91b7b
Didrik Munther <dmu0817@gmail.com>
2023-06-12 13:01:39 +0200

    Initial Commit

---
+++ BonCalc/BonCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonCalcApp.swift
+//  BonCalc
+//
+//  Created by Didrik Munther on 2023-06-12.
+//
+
+import SwiftUI
+
+@main
+struct BonCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
